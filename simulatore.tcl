579258e022ba9cb011b2caab48ac76aee87a29d2
qiubix <qiubix@gmail.com>
2014-05-27 17:17:12 +0200

    Configure simple simulator to work with ns.

--- simulatore.tcl
+++ simulatore.tcl
@@ -22,25 +22,25 @@ proc finish {} {
 
 #Create four nodes
 set n0 [$ns node]
-set n1 [$ns node]
-#set n2 [$ns node]
+#set n1 [$ns node]
+set n2 [$ns node]
 #set n3 [$ns node]
 
 #Create links between the nodes
-$ns duplex-link $n0 $n1 2Mb 10ms DropTail
+$ns duplex-link $n0 $n2 2Mb 10ms DropTail
 #$ns duplex-link $n1 $n2 2Mb 10ms DropTail
 #$ns duplex-link $n2 $n3 1.7Mb 20ms DropTail
 
 #Set Queue Size of link (n2-n3) to 10
-$ns queue-limit $n2 $n3 10
+#$ns queue-limit $n2 $n3 10
 
 #Give node position (for NAM)
-$ns duplex-link-op $n0 $n1 orient right-down
+$ns duplex-link-op $n0 $n2 orient right-down
 #$ns duplex-link-op $n1 $n2 orient right-up
 #$ns duplex-link-op $n2 $n3 orient right
 
 #Monitor the queue for link (n2-n3). (for NAM)
-$ns duplex-link-op $n2 $n3 queuePos 0.5
+#$ns duplex-link-op $n2 $n3 queuePos 0.5
 
 
 #Setup a TCP connection
@@ -48,7 +48,7 @@ set tcp [new Agent/TCP]
 $tcp set class_ 2
 $ns attach-agent $n0 $tcp
 set sink [new Agent/TCPSink]
-$ns attach-agent $n3 $sink
+$ns attach-agent $n2 $sink
 $ns connect $tcp $sink
 $tcp set fid_ 1
 
@@ -60,9 +60,9 @@ $ftp set type_ FTP
 
 #Setup a UDP connection
 set udp [new Agent/UDP]
-$ns attach-agent $n1 $udp
+$ns attach-agent $n0 $udp
 set null [new Agent/Null]
-$ns attach-agent $n3 $null
+$ns attach-agent $n2 $null
 $ns connect $udp $null
 $udp set fid_ 2
 
@@ -82,7 +82,7 @@ $ns at 4.0 "$ftp stop"
 $ns at 4.5 "$cbr stop"
 
 #Detach tcp and sink agents (not really necessary)
-$ns at 4.5 "$ns detach-agent $n0 $tcp ; $ns detach-agent $n3 $sink"
+$ns at 4.5 "$ns detach-agent $n0 $tcp ; $ns detach-agent $n2 $sink"
 
 #Call the finish procedure after 5 seconds of simulation time
 $ns at 5.0 "finish"
