f5fe240d0bb029c1be0e14d1e26939ce19af5005
qiubix <qiubix@gmail.com>
2014-05-28 12:27:48 +0200

    Add script with sample simulation as a base
    for simulation of vegas algorithm.

---
+++ task-vegas.tcl
@@ -0,0 +1,68 @@
+#Create a simulator object
+set ns [new Simulator]
+
+#Define different colors for data flows (for NAM)
+$ns color 1 Blue
+$ns color 2 Red
+
+#Open the NAM trace file
+set nf [open out.nam w]
+$ns namtrace-all $nf
+
+#Define a 'finish' procedure
+proc finish {} {
+        global ns nf
+        $ns flush-trace
+        #Close the NAM trace file
+        close $nf
+        #Execute NAM on the trace file
+        exec nam out.nam &
+        exit 0
+}
+
+#Create nodes
+set n0 [$ns node]
+set n1 [$ns node]
+
+#Create links between the nodes
+$ns duplex-link $n0 $n1 2mb 10ms DropTail
+
+#Give node position (for NAM)
+$ns duplex-link-op $n0 $n1 orient right
+
+#Setup a TCP connection
+set tcp [new Agent/TCP]
+$tcp set class_ 2
+$ns attach-agent $n0 $tcp
+set sink [new Agent/TCPSink]
+$ns attach-agent $n1 $sink
+$ns connect $tcp $sink
+$tcp set fid_ 1
+
+#Setup a UDP connection
+set udp [new Agent/UDP]
+$ns attach-agent $n0 $udp
+set null [new Agent/Null]
+$ns attach-agent $n1 $null
+$ns connect $udp $null
+$udp set fid_ 2
+
+#Setup a CBR over UDP connection
+set cbr [new Application/Traffic/CBR]
+$cbr attach-agent $udp
+$cbr set type_ CBR
+$cbr set packet_size_ 1000
+$cbr set rate_ 1mb
+$cbr set random_ false
+
+#Schedule events for the CBR and FTP agents
+$ns at 0.1 "$cbr start"
+$ns at 4.5 "$cbr stop"
+
+#Call the finish procedure after 5 seconds of simulation time
+$ns at 5.0 "finish"
+
+
+#Run the simulation
+$ns run
+
